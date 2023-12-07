461c87acc087fd953192c87dda1f9bdb91985b70
TheDankDork <150256710+TheDankDork@users.noreply.github.com>
2023-12-06 23:46:04 -0500

    Completed project
    Added comments for label which were forgotten earlier.

--- Label.java
+++ Label.java
@@ -1,7 +1,12 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
+/*
+ * Create a label class to display text.
+ * It is overloaded to take in two different types of inputs.
+ */
 public class Label extends Actor{
 
+    // Set variables and colours
     private String context = "";
     private String value;
     private int fontSize;
@@ -10,7 +15,7 @@ public class Label extends Actor{
     
     private static final Color transparent = new Color(0, 0, 0, 0);
 
-    public Label(String context, int value, int fontSize){
+    public Label(String context, int value, int fontSize){ // For score and lives display with a context and a number
         this.value = Integer.toString(value);
         this.context = context; 
         
@@ -18,33 +23,33 @@ public class Label extends Actor{
         updateImage();
     }
 
-    public Label(String value, int fontSize){
+    public Label(String value, int fontSize){ // For plain text
         this.value = value;
         this.fontSize = fontSize;
         updateImage();
     }
 
-    public void setValue(String value){
+    public void setValue(String value){ // Updates the value for a text
         this.value = value;
         updateImage();
     }
     
-    public void setValue(int value){
+    public void setValue(int value){ // Updates the value for an integer
         this.value = Integer.toString(value);
         updateImage();
     }
     
-    public void setLineColor(Color lineColor){
+    public void setLineColor(Color lineColor){ // Set the line colour of the text
         this.lineColor = lineColor;
         updateImage();
     }
     
-    public void setFillColor(Color fillColor){
+    public void setFillColor(Color fillColor){ // Set the fill colour of the text
         this.fillColor = fillColor;
         updateImage();
     }
     
-    private void updateImage(){
+    private void updateImage(){ // Update the image and refresh it with correct parameters
         value = this.context + this.value;
         setImage(new GreenfootImage(value, fontSize, fillColor, transparent, lineColor));
     }
